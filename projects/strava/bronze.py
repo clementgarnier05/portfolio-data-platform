28f969232c02af84ef289e27285f17cd2956f2f2
clementgarnier05 <clement.garnier@decathlon.com>
2025-06-23 10:31:18 +0200

    correct strava bronze spark builder

--- projects/strava/bronze.py
+++ projects/strava/bronze.py
@@ -9,11 +9,9 @@ from strava_auth import get_access_token, update_strava_tokens
 
 builder = (
     SparkSession.builder
-    .appName("StravaBronze")
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.2.0")
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    .getOrCreate()
+        .appName("StravaBronze")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 )
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
